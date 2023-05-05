777c66e9732dc466b111cb59ca7955b46638867b
Ecob1 <cob19007@byui.edu>
2023-05-04 17:58:54 -0700

    Updated .rest and server.js files

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-const mongodb = require('./db/connect');
-const bodyParser = require('body-parser');
+const mongodb = require("./db/connect");
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
-app.use('/', require('./routes'));
-app.use(bodyParser.json()).use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
     next();
   })
-  .use('/', require('./routes'));
+  .use("/", require("./routes"));
 mongodb.initDb((err, mongodb) => {
   if (err) {
     console.log(err);
