d9703954387b00c19f3d3aa7ce2de251993d0cf1
Ecob1 <cob19007@byui.edu>
2023-05-10 18:49:14 -0700

    new files

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
-router.use('/contacts', require('./contacts'))
-
+router.use("/", require("./swagger"));
+router.use("/contacts", require("./contacts"));
 module.exports = router;
